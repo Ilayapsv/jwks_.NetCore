a9738e82a97c4fc4815ff92ad1657deebecd2489
Ilayapsv <71894006+Ilayapsv@users.noreply.github.com>
2025-10-29 07:35:38 -0400

    Update EPICJWKController.cs

--- controllers/EPICJWKController.cs
+++ controllers/EPICJWKController.cs
@@ -14,7 +14,8 @@ namespace EPICJWK.Controllers
         public IActionResult GetJwks()
         {
             //string folderPath = Path.Combine(AppContext.BaseDirectory, "App_Data");
-            string folderPath = "/jwks_.NetCore/App_Data";
+            string folderPath = Environment.GetEnvironmentVariable("APP_DATA_PATH")
+                   ?? Path.Combine(builder.Environment.ContentRootPath, "App_Data");
 
             if (!Directory.Exists(folderPath))
                 return NotFound("App_Data folder not found.");
