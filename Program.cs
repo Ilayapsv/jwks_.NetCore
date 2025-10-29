7e0b995f0a88aeb5e37bf564221fe2ea7aa80583
Ilayapsv <71894006+Ilayapsv@users.noreply.github.com>
2025-10-29 08:25:14 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(sp =>
+{
+    var env = sp.GetRequiredService<IWebHostEnvironment>();
+    return Path.Combine(env.ContentRootPath, "App_Data");
+});
 
 var app = builder.Build();
 
@@ -23,3 +28,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
